84ff9a7a7911ae74b91298742a74d50587e3673a
elviswangari <elviswangare@gmail.com>
2023-07-06 20:07:52 +0300

    to str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+a module to return a string
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    function that returns a string version of n
+    '''
+    return str(n)
