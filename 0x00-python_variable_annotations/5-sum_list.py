068210dc18855ffefcbf92a03963b43f840de68b
elviswangari <elviswangare@gmail.com>
2023-07-06 20:26:04 +0300

    complex types

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+module to return sum of list
+'''
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''
+    function that takes in list and returns floats
+    '''
+    return float(sum(input_list))
