f63c1fe985784685e0727101888970c2b0dfb9cc
elviswangari <elviswangare@gmail.com>
2023-07-06 21:08:32 +0300

    tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -7,7 +7,7 @@ module for var with multiple types
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[int, float]:
     """
     retuns a tuple of string and square of either int or tuple
     """
