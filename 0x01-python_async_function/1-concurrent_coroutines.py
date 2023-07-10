3ac933ee34a7a8f36b4bf8ca87d42d07294ff5bf
elviswangari <elviswangare@gmail.com>
2023-07-10 20:40:36 +0300

    multiple coroutines

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+module for calling multiple async
+'''
+from typing import List
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    a coroutine module that returns a list of delays
+
+    Args:
+        n: an int to repeesent number of times
+        max_delay: maximum delays
+
+    Returns:
+        a lists of flaots
+    '''
+    wait_time = await asyncio.gather(
+            *tuple(map(lambda _: wait_random(max_delay), range(n)))
+        )
+    return wait_time
