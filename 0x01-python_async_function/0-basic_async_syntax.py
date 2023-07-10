3d9b8cdd4570f49e1844283ad8ae6087aeaf4a07
elviswangari <elviswangare@gmail.com>
2023-07-10 20:05:37 +0300

    async coroutines

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,12 +3,13 @@
 working with async
 '''
 from random import uniform
-from typing import Union
+import asyncio
 
-
-async def wait_random(max_delay: int = 10) -> Union[int, float]:
+async def wait_random(max_delay: int = 10) -> float:
     '''
     a async function that takes in an int arg and waits from 0 to max_delay
     then returns an int or float
     '''
-    return uniform(0, max_delay)
+    wait_time = uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+    return wait_time
