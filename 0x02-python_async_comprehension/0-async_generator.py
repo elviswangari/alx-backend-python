a90c0fd30a11228e569674b94ae74da63c413ff1
elviswangari <elviswangare@gmail.com>
2023-07-11 22:54:08 +0300

    async generators

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,6 +10,9 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator:
     '''
     an async generator that yields a random number
+
+    Return:
+        an async generator
     '''
     for _ in range(10):
         await asyncio.sleep(1)
